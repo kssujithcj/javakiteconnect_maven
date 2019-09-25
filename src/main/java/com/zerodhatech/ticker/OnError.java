096bbddf1098b1f977fcef33725cedf209d23f3e
Sujith <kssujithcj@gmail.com>
2019-09-25 14:07:48 +0530

    initial commit for javakiteconnect_maven

---
+++ src/main/java/com/zerodhatech/ticker/OnError.java
@@ -0,0 +1,15 @@
+package com.zerodhatech.ticker;
+
+import com.zerodhatech.kiteconnect.kitehttp.exceptions.KiteException;
+
+/**
+ * Created by sujith on 11/21/17.
+ */
+public interface OnError {
+
+    public void onError(Exception exception);
+
+    public void onError(KiteException kiteException);
+
+    void onError(String error);
+}
