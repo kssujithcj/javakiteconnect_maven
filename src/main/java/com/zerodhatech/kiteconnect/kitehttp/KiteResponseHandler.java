b4e4da15f64fd3ff722f2964ab91ef4fedcad609
Sujith <kssujithcj@gmail.com>
2023-07-28 15:13:13 +0530

    feat: changes to read local body string rather than reading response stream again

--- src/main/java/com/zerodhatech/kiteconnect/kitehttp/KiteResponseHandler.java
+++ src/main/java/com/zerodhatech/kiteconnect/kitehttp/KiteResponseHandler.java
@@ -21,7 +21,7 @@ public class KiteResponseHandler {
             }
             return jsonObject;
         } else {
-            throw new DataException("Unexpected content type received from server: "+ response.header("Content-Type")+" "+response.body().string(), 502);
+            throw new DataException("Unexpected content type received from server: "+ response.header("Content-Type")+" "+body, 502);
         }
     }
 
@@ -29,9 +29,9 @@ public class KiteResponseHandler {
         if (response.header("Content-Type").contains("csv")) {
             return body;
         } else if(response.header("Content-Type").contains("json")){
-            throw dealWithException(new JSONObject(response.body().string()), response.code());
+            throw dealWithException(new JSONObject(body), response.code());
         } else {
-            throw new DataException("Unexpected content type received from server: "+ response.header("Content-Type")+" "+response.body().string(), 502);
+            throw new DataException("Unexpected content type received from server: "+ response.header("Content-Type")+" "+body, 502);
         }
     }
 
