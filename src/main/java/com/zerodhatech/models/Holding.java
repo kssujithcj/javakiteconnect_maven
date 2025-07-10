95c49d487a80410973599a67dbfd3b7570828db5
Sujith K S <kssujithcj@gmail.com>
2025-07-10 17:24:01 +0530

    feat: added mtf holding data to holdings model

--- src/main/java/com/zerodhatech/models/Holding.java
+++ src/main/java/com/zerodhatech/models/Holding.java
@@ -48,4 +48,6 @@ public class Holding {
     public  double dayChange;
     @SerializedName("day_change_percentage")
     public  double dayChangePercentage;
+    @SerializedName("mtf")
+    public  MTFHolding mtf;
 }
